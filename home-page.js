a8dbf1119aab6c7222668076917e2d1a19987e00
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2025-03-20 23:25:54 +0200

    Adding Javascript file

---
+++ home-page.js
@@ -0,0 +1,21 @@
+function getSelectedValue(name) {
+    const selected = document.querySelector(`input[name="${name}"]:checked`);
+    return selected ? selected.value : null;
+}
+
+function feelings() {
+    const moods = ["happy", "sad", "tired", "energized", "stressed", "angry"];
+    const moodValues = {};
+
+    moods.forEach(mood => {
+        moodValues[mood] = getSelectedValue(mood);
+    });
+
+    const allZero = Object.values(moodValues).every(value => value === "0" || value === null);
+
+    if (allZero) {
+        alert("Please enter your feelings.");
+    } else {
+        alert("Feelings have been entered:\n" + JSON.stringify(moodValues, null, 2));
+    }
+}
